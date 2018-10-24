3de1f117cb7ae896ee644fd58c242dd64e91fd43
nkhanhng97@gmail.com <khanhmatden123@gmail.com>
2018-10-24 18:43:19 +0700

    fix

--- client/src/component/post/CommentItem.js
+++ client/src/component/post/CommentItem.js
@@ -17,7 +17,7 @@ class CommentItem extends Component {
                     <div className="row">
                         <div className="col-md-2">
                             <a href="profile.html">
-                                <img className="rounded-circle d-none d-md-block" src={comment.avatar}/>
+                                <img className="rounded-circle d-none d-md-block" src={comment.avatar} alt="avatar"/>
                             </a>
                             <br />
                             <p className="text-center">{comment.name}</p>
--- client/src/store.js
+++ client/src/store.js
@@ -1,16 +1,21 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers';
+import {composeWith} from 'redux'
 
 const initialState = {};
 const middleware = [thunk];
+
+const devTools = process.env.NODE_ENV === 'development' 
+                    ? window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() 
+                    : null
 
 const store = createStore(
     rootReducer,
     initialState, 
     compose(
         applyMiddleware(...middleware),
-        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+        devTools
     )
 )
 
